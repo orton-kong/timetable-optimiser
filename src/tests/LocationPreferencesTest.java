d41f6a3c8afa19c88e70b39aace4dad3d92ea5cf
wann0038 <wann0038@flinders.edu.au>
2026-06-04 02:17:44 +0930

    Add unit test for LocationPreferences initialization

---
+++ src/tests/LocationPreferencesTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import timetableoptimizer.LocationPreferences;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationPreferencesTest {
+
+    @Tag("Akarsh")
+    @Tag("Additional")
+    @DisplayName("Location Preferences Initialise")
+    @Test
+    void locationPreferencesInitialise() {
+        assertArrayEquals(
+                new LocationPreferences[]{
+                        LocationPreferences.BEDFORD,
+                        LocationPreferences.TONSLEY,
+                        LocationPreferences.CITY,
+                        LocationPreferences.ALL_AT_SAME_CAMPUS
+                },
+                LocationPreferences.values()
+        );
+    }
+}

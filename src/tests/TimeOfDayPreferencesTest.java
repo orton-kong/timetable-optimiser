6f1f6948889d6c878b7f29b70d757e8d33f498b7
wann0038 <wann0038@flinders.edu.au>
2026-06-04 02:19:04 +0930

    Create TimeOfDayPreferencesTest.java

---
+++ src/tests/TimeOfDayPreferencesTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import timetableoptimizer.TimeOfDayPreferences;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeOfDayPreferencesTest {
+
+    @Tag("Akarsh")
+    @Tag("Additional")
+    @DisplayName("Time Of Day Preferences Initialise")
+    @Test
+    void timeOfDayPreferencesInitialise() {
+        assertArrayEquals(
+                new TimeOfDayPreferences[]{
+                        TimeOfDayPreferences.MORNING,
+                        TimeOfDayPreferences.AFTERNOON
+                },
+                TimeOfDayPreferences.values()
+        );
+    }
+}

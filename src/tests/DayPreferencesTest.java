02b9dcd5466b6efcf49eef04b3de8ab2dde0f757
wann0038 <wann0038@flinders.edu.au>
2026-06-04 02:18:27 +0930

    Add unit test for DayPreferences initialization

---
+++ src/tests/DayPreferencesTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import timetableoptimizer.DayPreferences;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayPreferencesTest {
+
+    @Tag("Akarsh")
+    @Tag("Core")
+    @DisplayName("Day Preferences Initialise")
+    @Test
+    void dayPreferencesInitialise() {
+        assertArrayEquals(
+                new DayPreferences[]{
+                        DayPreferences.MONDAY,
+                        DayPreferences.TUESDAY,
+                        DayPreferences.WEDNESDAY,
+                        DayPreferences.THURSDAY,
+                        DayPreferences.FRIDAY
+                },
+                DayPreferences.values()
+        );
+    }
+}
